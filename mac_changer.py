d753c8bf43994b096d625bda9ec02f7373cc2108
Tinashe Chihoro <tinashechihoro@gmail.com>
2020-10-14 17:06:10 +0200

    using optparse for argument processing

--- mac_changer.py
+++ mac_changer.py
@@ -1,6 +1,18 @@
 #!/usr/bin/env python
-import  subprocess
+import subprocess
+import optparse
 
-subprocess.call('ifconfig wlan0 down',shell=True)
-subprocess.call('ifconfig wlan0 hw ether 11:22:44:55:66:77',shell=True)
-subprocess.call('ifconfig wlan0 up',shell=True)
+parser = optparse.OptionParser()
+
+parser.add_option('-i','--interface',dest='interface',help='Interface to change the MAC address')
+parser.add_option('-m','--mac',dest='new_mac',help='New MAC address')
+( options, arguments ) = parser.parse_args()
+
+interface = options.interface
+new_mac = options.new_mac
+
+print('[+] Changing MAC address for  ' + interface + " to " + new_mac)
+subprocess.call(['ifconfig', interface, 'down'])
+subprocess.call(['ifconfig', interface, 'down'])
+subprocess.call(['ifconfig', interface, 'hw ether', new_mac])
+subprocess.call(['ifconfig', interface, 'up'])
